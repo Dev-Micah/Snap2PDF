6f67bdd6da103427f6da78e25e09485bac4a2d51
Micah Nyabuto <atembamicah@gmail.com>
2025-07-12 00:21:01 +0300

    Extended material icons dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Extended material3 icons
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
 }
