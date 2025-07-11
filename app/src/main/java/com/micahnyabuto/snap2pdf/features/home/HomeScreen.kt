d0f2b6eaf5692baf7046b272482735a7e0ec324b
Micah Nyabuto <atembamicah@gmail.com>
2025-07-12 00:23:00 +0300

    HomeScreen Topbar

---
+++ app/src/main/java/com/micahnyabuto/snap2pdf/features/home/HomeScreen.kt
@@ -0,0 +1,54 @@
+package com.micahnyabuto.snap2pdf.features.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen(){
+    HomeScreenContent()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreenContent(){
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Scan2PDF",
+                        fontWeight = FontWeight.Bold)
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = "Profile",
+                            tint = Color.Black,
+                            modifier = Modifier.size(35.dp)
+                                .clip(CircleShape)
+                                .background(Color.LightGray)
+                        )
+                    }
+                }
+            )
+        }
+    ){
+
+    }
+}
