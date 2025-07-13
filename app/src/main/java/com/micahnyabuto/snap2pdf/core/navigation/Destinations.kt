2789c16980d900b8db1d15a669c04c5d974391a6
Micah Nyabuto <atembamicah@gmail.com>
2025-07-13 16:22:46 +0300

    update

--- app/src/main/java/com/micahnyabuto/snap2pdf/core/navigation/Destinations.kt
+++ app/src/main/java/com/micahnyabuto/snap2pdf/core/navigation/Destinations.kt
@@ -3,7 +3,12 @@ package com.micahnyabuto.snap2pdf.core.navigation
 sealed class Destinations(val route: String){
     object Home: Destinations("home")
     object History: Destinations("history")
+
     object Settings: Destinations("settings")
-    object Profile: Destinations("profile")
+
+    object Files: Destinations("files")
+
     object Splash: Destinations("splash")
+
+    object Profile: Destinations("profile")
 }
