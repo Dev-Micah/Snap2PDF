54d2d427baab54f8b0be6dbb1011ed543c4cf9cc
Micah Nyabuto <atembamicah@gmail.com>
2025-07-13 16:28:10 +0300

    Added Greeting function based on time

---
+++ app/src/main/java/com/micahnyabuto/snap2pdf/utils/Greeting.kt
@@ -0,0 +1,25 @@
+package com.micahnyabuto.snap2pdf.utils
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import java.util.Calendar
+
+
+@Composable
+fun Greeting(){
+    val greetingText = greetingMessage()
+
+    Text(text = greetingText)
+
+}
+
+fun greetingMessage():String{
+    val hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+
+    return when (hour){
+        in 0..5 -> "Good Night "
+        in 6..11 -> "Good Morning"
+        in 12..17 -> "Good Afternoon"
+        else -> "Good Evening"
+    }
+}
