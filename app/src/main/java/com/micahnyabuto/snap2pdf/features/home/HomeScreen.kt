eb3043a265f9db6b7e93b7c0609936badcba2fb3
Micah Nyabuto <atembamicah@gmail.com>
2025-07-13 16:24:42 +0300

    Added Greeting

--- app/src/main/java/com/micahnyabuto/snap2pdf/features/home/HomeScreen.kt
+++ app/src/main/java/com/micahnyabuto/snap2pdf/features/home/HomeScreen.kt
@@ -1,6 +1,14 @@
 package com.micahnyabuto.snap2pdf.features.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -8,19 +16,33 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.micahnyabuto.snap2pdf.R
+import com.micahnyabuto.snap2pdf.utils.Greeting
 
 @Composable
 fun HomeScreen(){
@@ -30,14 +52,24 @@ fun HomeScreen(){
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreenContent(){
+    var text by remember { mutableStateOf("") }
+    val isDarkMode = isSystemInDarkTheme()
+    val textColor = if (isDarkMode) Color.DarkGray else Color.LightGray
     Scaffold (
         topBar = {
             TopAppBar(
                 title = {
-                    Text("Snap2PDF",
-                        fontWeight = FontWeight.Bold)
+                    Greeting()
+
                 },
                 actions = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "search file",
+                            modifier = Modifier.size(28.dp)
+                        )
+                    }
                     IconButton(onClick = {}) {
                         Icon(
                             imageVector = Icons.Default.Person,
@@ -54,7 +86,8 @@ fun HomeScreenContent(){
         floatingActionButton = {
             FloatingActionButton(onClick = {},
                 modifier = Modifier.padding(bottom = 80.dp),
-                shape = RoundedCornerShape(100.dp)
+                shape = RoundedCornerShape(100.dp),
+                containerColor = MaterialTheme.colorScheme.primary
             ) {
                 Icon(
                     imageVector = Icons.Default.CameraAlt,
