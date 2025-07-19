9427f43a5de37ae7c3603e7f1b752f80be777e60
Micah Nyabuto <atembamicah@gmail.com>
2025-07-19 13:39:42 +0300

    Update

--- app/src/main/java/com/micahnyabuto/snap2pdf/MainActivity.kt
+++ app/src/main/java/com/micahnyabuto/snap2pdf/MainActivity.kt
@@ -4,38 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.only
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.surfaceColorAtElevation
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import com.micahnyabuto.snap2pdf.core.navigation.AppNavHost
-import com.micahnyabuto.snap2pdf.core.navigation.BottomNavigation
-import com.micahnyabuto.snap2pdf.core.navigation.Destinations
-import com.micahnyabuto.snap2pdf.features.home.HomeScreen
 import com.micahnyabuto.snap2pdf.ui.theme.Snap2PDFTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Snap2PDFTheme {
-                    AppNavHost()
+                AppNavHost()
                 }
             }
         }
