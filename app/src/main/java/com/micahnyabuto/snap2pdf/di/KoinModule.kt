0d0146fa20e40488b92adc0623f58134895ddf51
Micah Nyabuto <atembamicah@gmail.com>
2025-07-25 23:27:18 +0300

    Added KoinModule

--- app/src/main/java/com/micahnyabuto/snap2pdf/di/KoinModule.kt
+++ app/src/main/java/com/micahnyabuto/snap2pdf/di/KoinModule.kt
@@ -1,8 +1,22 @@
 package com.micahnyabuto.snap2pdf.di
 
 
+import androidx.room.Room
+import com.micahnyabuto.snap2pdf.core.data.local.DocumentDatabase
+import com.micahnyabuto.snap2pdf.core.data.repository.DocumentRepository
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val databaseModule = module {
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            DocumentDatabase::class.java,
+            "documents_db"
+        ).build()
+    }
 
+    single { get<DocumentDatabase>().documentDao() }
+
+    single { DocumentRepository(get()) }
 }
