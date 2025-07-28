8efca352c803b19ff436244e48a50f36fa6598fd
Micah Nyabuto <atembamicah@gmail.com>
2025-07-28 23:26:40 +0300

    Added Repository and RepositoryImpl

---
+++ app/src/main/java/com/micahnyabuto/snap2pdf/core/data/repository/DocumentRepository.kt
@@ -0,0 +1,25 @@
+package com.micahnyabuto.snap2pdf.core.data.repository
+
+import com.micahnyabuto.snap2pdf.core.data.local.Document
+import com.micahnyabuto.snap2pdf.core.data.local.DocumentDao
+import kotlinx.coroutines.flow.Flow
+
+interface DocumentRepository {
+    suspend fun addDocument(document: Document)
+    suspend fun deleteDocument(document: Document)
+
+    fun allDocuments(document: Document): Flow<List<Document>>
+}
+
+class DocumentRepositoryImpl(private val dao: DocumentDao): DocumentRepository{
+
+    override suspend fun addDocument(document: Document) {
+        dao.insert(document)
+    }
+
+    override suspend fun deleteDocument(document: Document) {
+        dao.delete(document)
+    }
+
+    override fun allDocuments(document: Document): Flow<List<Document>> = dao.allDocuments()
+}
