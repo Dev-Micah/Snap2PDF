f5d0dd6f1815f34b483d194cc623b3b2076fbfcc
Micah Nyabuto <atembamicah@gmail.com>
2025-08-27 17:31:52 +0300

    Update

--- app/src/main/java/com/micahnyabuto/snap2pdf/MainActivity.kt
+++ app/src/main/java/com/micahnyabuto/snap2pdf/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.micahnyabuto.snap2pdf
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import com.micahnyabuto.snap2pdf.core.data.local.Document
 import com.micahnyabuto.snap2pdf.core.navigation.AppNavHost
 import com.micahnyabuto.snap2pdf.ui.theme.Snap2PDFTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("ViewModelConstructorInComposable")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Snap2PDFTheme {
-                AppNavHost()
-                }
+                AppNavHost(
+                    activity = this,
+                )
             }
-        }
-}
 
+        }
+
+    }
+
+}
