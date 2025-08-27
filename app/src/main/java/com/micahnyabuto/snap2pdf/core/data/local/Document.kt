80b681658337c8ec7f580bc63dcce90fb966787b
Micah Nyabuto <atembamicah@gmail.com>
2025-08-27 22:47:44 +0300

    Update

--- app/src/main/java/com/micahnyabuto/snap2pdf/core/data/local/Document.kt
+++ app/src/main/java/com/micahnyabuto/snap2pdf/core/data/local/Document.kt
@@ -1,13 +1,15 @@
 package com.micahnyabuto.snap2pdf.core.data.local
 
+import androidx.room.ColumnInfo // Added import
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "documents")
 data class Document(
     @PrimaryKey(autoGenerate = true)
-    val id: Int= 0,
+    val id: Int = 0,
     val name: String,
     val uri: String,
-    val createdAt: Long = System.currentTimeMillis()
+    val createdAt: Long = System.currentTimeMillis(),
+    //val isImportant: Boolean = false
 )
