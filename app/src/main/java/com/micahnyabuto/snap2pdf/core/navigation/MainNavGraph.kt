5956eafbe4424163214e8c6801cbdfef6e407234
Micah Nyabuto <atembamicah@gmail.com>
2025-08-28 22:21:17 +0300

    Moved Scanner FAB to bottom bar

--- app/src/main/java/com/micahnyabuto/snap2pdf/core/navigation/MainNavGraph.kt
+++ app/src/main/java/com/micahnyabuto/snap2pdf/core/navigation/MainNavGraph.kt
@@ -4,16 +4,26 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.net.Uri
 import android.os.Build
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DocumentScanner
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -26,6 +36,7 @@ import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,6 +44,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanning
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
 import com.micahnyabuto.snap2pdf.features.files.FilesScreen
 import com.micahnyabuto.snap2pdf.features.home.HomeScreen
 import com.micahnyabuto.snap2pdf.features.scanner.SavePDFScreen
@@ -56,14 +70,31 @@ fun MainNavGraph(
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route.orEmpty()
 
+    val context = LocalContext.current
+
+    //scannerLauncher
+    val scannerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartIntentSenderForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val scanResult = GmsDocumentScanningResult.fromActivityResultIntent(result.data)
+            val imageUris = scanResult?.pages?.mapNotNull { it.imageUri } ?: emptyList()
+            val pdfUri = scanResult?.pdf?.uri
+
+            scannerViewModel.setScanResults(pdfUri, imageUris)
+            navController.navigate(Destinations.Save.route)
+        }
+    }
+
+
 
 
     val showBottomNavigation = currentRoute !in listOf(
         Destinations.Splash.route,
         Destinations.Search.route,
         Destinations.Snap.route,
-        Destinations.Preview.route,
-        Destinations.Save.route
+        Destinations.Save.route,
+        Destinations.View.route
     )
 
     Scaffold(
@@ -74,50 +105,91 @@ fun MainNavGraph(
                 Column {
                     HorizontalDivider(thickness = 0.5.dp)
                     NavigationBar(
-                        tonalElevation = 0.dp,
-                        containerColor = MaterialTheme.colorScheme.surface
+                        containerColor = MaterialTheme.colorScheme.surfaceVariant
                     ) {
-                        BottomNavigation.entries.forEach { navigationItem ->
-
-                            val isSelected = currentRoute == navigationItem.route
-
+                        // Left side items (first two)
+                        BottomNavigation.entries.take(1).forEach { navigationItem ->
                             NavigationBarItem(
-                                selected = isSelected,
-                                icon = {
-                                    Icon(
-                                        imageVector = if (isSelected) navigationItem.selectedIcon else navigationItem.unselectedIcon,
-                                        contentDescription = navigationItem.label
-                                    )
-                                },
-                                label = {
-                                    Text(
-                                        text = navigationItem.label,
-                                        style = MaterialTheme.typography.labelSmall.copy(
-                                            fontSize = 10.sp,
-                                            fontWeight = if (isSelected) FontWeight.SemiBold else FontWeight.Normal
-                                        )
-                                    )
-                                },
-                                onClick = {
-                                    if (currentRoute != navigationItem.route) {
-                                        navController.navigate(navigationItem.route)
-                                    }
-                                },
+                                selected = currentRoute == navigationItem.route,
+                                onClick = { navController.navigate(navigationItem.route) },
                                 colors = NavigationBarItemDefaults.colors(
-                                    indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
-                                        elevation = 0.dp
-                                    ),
+                                    indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(0.dp),
                                     selectedIconColor = MaterialTheme.colorScheme.primary,
                                     selectedTextColor = MaterialTheme.colorScheme.primary,
-                                    unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                                    unselectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant
-                                )
+                                    unselectedIconColor = MaterialTheme.colorScheme.onSurface,
+                                    unselectedTextColor = MaterialTheme.colorScheme.onSurface
+                                ),
+                                icon = {
+                                    Icon(
+                                        imageVector = navigationItem.unselectedIcon,
+                                        contentDescription = navigationItem.label,
+                                        modifier = Modifier.size(24.dp)
+                                    )
+                                },
+                                label = { Text(navigationItem.label, fontSize = 10.sp) }
+                            )
+                        }
+
+                        // Spacer in the middle for FAB
+                        FloatingActionButton(
+                            onClick = {
+
+                                val options = GmsDocumentScannerOptions.Builder()
+                                    .setGalleryImportAllowed(true)
+                                    .setPageLimit(5)
+                                    .setResultFormats(GmsDocumentScannerOptions.RESULT_FORMAT_JPEG, GmsDocumentScannerOptions.RESULT_FORMAT_PDF)
+                                    .setScannerMode(GmsDocumentScannerOptions.SCANNER_MODE_FULL)
+                                    .build()
+
+                                val scanner = GmsDocumentScanning.getClient(options)
+                                scanner.getStartScanIntent(activity)
+                                    .addOnSuccessListener { intentSender ->
+                                        val request = IntentSenderRequest.Builder(intentSender).build()
+                                        scannerLauncher.launch(request)
+                                    }
+                                    .addOnFailureListener {
+                                        Toast.makeText(context, "Scanner failed to launch", Toast.LENGTH_SHORT).show()
+                                    }
+                            },
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            contentColor = MaterialTheme.colorScheme.onPrimary,
+                            shape = RoundedCornerShape(50.dp),
+                            modifier = Modifier.padding(10.dp)
+                                .offset(0.dp, -11.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.DocumentScanner,
+                                contentDescription = "Add",
+                                tint = MaterialTheme.colorScheme.onPrimary
+                            )
+
+                        }
+
+                        // Right side items (last two)
+                        BottomNavigation.entries.takeLast(1).forEach { navigationItem ->
+                            NavigationBarItem(
+                                selected = currentRoute == navigationItem.route,
+                                onClick = { navController.navigate(navigationItem.route) },
+                                colors = NavigationBarItemDefaults.colors(
+                                    indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(0.dp),
+                                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                                    selectedTextColor = MaterialTheme.colorScheme.primary,
+                                    unselectedIconColor = MaterialTheme.colorScheme.onSurface,
+                                    unselectedTextColor = MaterialTheme.colorScheme.onSurface
+                                ),
+                                icon = {
+                                    Icon(
+                                        imageVector = navigationItem.unselectedIcon,
+                                        contentDescription = navigationItem.label,
+                                        modifier = Modifier.size(24.dp)
+                                    )
+                                },
+                                label = { Text(navigationItem.label, fontSize = 10.sp) }
                             )
                         }
                     }
 
                 }
-
 
             }
         }
@@ -131,17 +203,18 @@ fun MainNavGraph(
             composable(Destinations.Home.route){
                 HomeScreen(
                     navController=navController,
-                    scannerLauncher = scannerLauncher,
                     activity = activity,
                     scannerViewModel = scannerViewModel,
                 )
             }
             composable(Destinations.Settings.route){
-                SettingsScreen()
+                SettingsScreen(
+                    viewModel = koinViewModel()
+                )
             }
-            composable(Destinations.Files.route){
-                FilesScreen()
-            }
+//            composable(Destinations.Files.route){
+//                FilesScreen()
+//            }
             composable(Destinations.Search.route){
                 SearchScreen(
                     navController=navController
@@ -156,6 +229,18 @@ fun MainNavGraph(
                     }
                 )
             }
+//            composable(
+//                Destinations.View.route + "view?uri={uri}",
+//                arguments = listOf(navArgument("uri") { type = NavType.StringType })
+//            ) { backStackEntry ->
+//                val uriString = backStackEntry.arguments?.getString("uri")
+//                val uri = uriString?.let { Uri.parse(it) }
+//                if (uri != null) {
+//                    PdfViewerScreen(documentUri = uri)
+//                }
+//            }
+
+
 
         }
     }
