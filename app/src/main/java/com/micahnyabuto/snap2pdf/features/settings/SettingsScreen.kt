d6da2611632e728fffdfa9586d7337119edb33fc
Micah Nyabuto <atembamicah@gmail.com>
2025-09-04 23:55:38 +0300

    Adde Socials

--- app/src/main/java/com/micahnyabuto/snap2pdf/features/settings/SettingsScreen.kt
+++ app/src/main/java/com/micahnyabuto/snap2pdf/features/settings/SettingsScreen.kt
@@ -1,5 +1,8 @@
 package com.micahnyabuto.snap2pdf.features.settings
 
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,29 +12,46 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.NavigateNext
 import androidx.compose.material.icons.filled.PrivacyTip
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.micahnyabuto.snap2pdf.R
 import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -41,6 +61,19 @@ fun SettingsScreen(
     viewModel: SettingsViewModel = koinViewModel()
 ) {
     val isDarkMode by viewModel.isDarkMode.collectAsState()
+
+    var showDialog by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        AboutDialog(onDismiss = { showDialog = false })
+    }
+    var showMessageDialog by remember { mutableStateOf(false) }
+
+    if (showMessageDialog) {
+        RequestFeatureDialog(onDismiss = { showMessageDialog = false })
+    }
+
+    val context = LocalContext.current
 
     Scaffold(
         topBar = {
@@ -114,42 +147,144 @@ fun SettingsScreen(
                     }
                 }
             }
-            // Privacy
-            SettingCard {
+            // About
+            Card(
+                modifier = Modifier.fillMaxWidth()
+                    .clickable{
+                        showDialog = true
+                    },
+                shape = RoundedCornerShape(12.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant
+                )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 12.dp)
+                ){
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Text("Data and privacy")
-                    IconButton(
-                        onClick = { /* Handle share action */ }
+                    Text("About Snap2PDF")
+                    Icon(
+                        imageVector = Icons.Default.Info,
+                        contentDescription = "About"
+                    )
+
+                }
+            }
+        }
+            // Privacy
+            Card(
+                modifier = Modifier.fillMaxWidth()
+                    .clickable {},
+                shape = RoundedCornerShape(12.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant
+                )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 12.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
                     ) {
+                        Text("Privacy Policy")
                         Icon(
                             imageVector = Icons.Default.PrivacyTip,
                             contentDescription = "Privacy"
                         )
+
                     }
                 }
             }
+            //Request a feature
+            Card(
+                modifier = Modifier.fillMaxWidth()
+                    .clickable{
+                        showMessageDialog = true
+                    },
+                shape = RoundedCornerShape(12.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant
+                )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 12.dp)
+                ){
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text("Request a feature")
+                    Icon(
+                        imageVector = Icons.Default.NavigateNext,
+                        contentDescription = "Privacy"
+                    )
+
+                }
+            }
+        }
             SettingCard {
                 Column(
                     modifier = Modifier.fillMaxWidth(),
                     verticalArrangement = Arrangement.spacedBy(4.dp)
                 ) {
-                   Text("About Developer")
+                   Text("How to reach Us")
                     Spacer(modifier = Modifier.height(4.dp))
 
                     Row(
                         modifier = Modifier.fillMaxWidth()
                                            .padding(start =8.dp),
                         verticalAlignment = Alignment.CenterVertically,
-                        //horizontalArrangement = Arrangement.SpaceBetween
                     ) {
-                        IconButton(onClick = {}) {
-                            Icon(imageVector = Icons.Default.Email, contentDescription = "Email")
+                        IconButton(onClick = {
+                            val intent = Intent(
+                                Intent.ACTION_VIEW,
+                                Uri.parse("https://x.com/Snap_2PDF?t=y67fbcpCWyz45yf_Z_w_Lg&s=09")
+                            )
+                            context.startActivity(intent)
+                        }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.twitter),
+                                contentDescription = "Twitter",
+                                tint = Color.Unspecified,
+                                modifier = Modifier.size(24.dp)
+
+                            )
                         }
-                        Text("atembamicah@gmail.com")
+                        Text("Follow us on Twitter")
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                                           .padding(start =8.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                        IconButton(onClick = {
+                            val intent = Intent(
+                                Intent.ACTION_VIEW,
+                                Uri.parse("mailto:atembamicah@gmail.com")
+                            )
+                            context.startActivity(intent)
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Email,
+                                contentDescription = "Email",
+                                modifier = Modifier.size(24.dp)
+
+                            )
+                        }
+                        Text("Developer")
                     }
                 }
             }
@@ -175,3 +310,105 @@ fun SettingCard(content: @Composable () -> Unit) {
         }
     }
 }
+@Composable
+fun AboutDialog(
+    onDismiss: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = { onDismiss() },
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.applogo), // Replace with your app icon
+                    contentDescription = null,
+                    tint = Color.Unspecified,
+                    modifier = Modifier.size(32.dp)
+                        .clip(RoundedCornerShape(4.dp))
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Column {
+                    Text(
+                        text = "Snap2PDF",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Text(
+                        text = "1.0.0",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+            }
+        },
+        text = {
+            Text(
+                text = "Snap2PDF makes it simple to turn your images into professional PDF files,Your pocket friendly Image -> PDF converter.",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        },
+        confirmButton = {
+            TextButton(
+                onClick = { onDismiss() }
+            ) {
+                Text("Close")
+            }
+        }
+    )
+}
+
+@Composable
+fun RequestFeatureDialog(
+    onDismiss: () -> Unit
+){
+    var message by remember { mutableStateOf("") }
+    AlertDialog(
+        onDismissRequest = { onDismiss() },
+        title = {
+            Column {
+                Text("Send Message",
+                    style = MaterialTheme.typography.titleMedium
+                )
+
+                Spacer(Modifier.height(8.dp))
+
+                OutlinedTextField(
+                    value = message,
+                    onValueChange = {message = it},
+                    modifier = Modifier.fillMaxWidth()
+                        .height(150.dp),
+                    placeholder = {Text("Type your message...",
+                        style = MaterialTheme.typography.bodyMedium)},
+                    shape = RoundedCornerShape(12.dp),
+                    maxLines = 4,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent,
+                        disabledContainerColor = Color.Transparent,
+                        focusedBorderColor = Color.LightGray,
+                        unfocusedBorderColor = Color.LightGray,
+                        disabledBorderColor = Color.Transparent
+                    )
+
+                )
+            }
+        },
+        confirmButton = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                TextButton(onClick = { onDismiss() }) {
+                    Text("Cancel")
+                }
+                Spacer(Modifier.width(8.dp))
+                Button(onClick = { onDismiss() }) {
+                    Text("Send")
+                }
+            }
+
+        }
+
+    )
+
+}
+
